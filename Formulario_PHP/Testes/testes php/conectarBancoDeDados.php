09fd3dc167f30a1e3534c57f703954b81500eced
movingGreen <el_vailant@hotmail.com>
2022-08-15 13:47:29 -0400

    tentando criar outras tabelas

--- Formulario_PHP/Testes/testes php/conectarBancoDeDados.php
+++ Formulario_PHP/Testes/testes php/conectarBancoDeDados.php
@@ -11,9 +11,7 @@ $bdConection = mysqli_connect($servername, $username, $password, $nomeBD);
 if (!$bdConection) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-
-var_dump($bdConection);
+echo "Connected successfully\n";
 
 // Criar banco de dados
 // $sqlCriarBanco = "CREATE DATABASE bd_teste_php";
@@ -31,7 +29,42 @@ var_dump($bdConection);
 //   cpf CHAR(11) NOT NULL
 //   )";
 
-$query = "EXPLAIN cliente";
+// $query = "CREATE TABLE PRODUTO (
+//   ID_Produto INTEGER PRIMARY KEY,
+//   VL_Unitario NUMERIC(5,2),
+//   Descricao VARCHAR(50),
+//   DT_Validade DATE,
+//   DT_Fabricacao DATE,
+//   Lote INTEGER,
+//   QTD_Estoque INTEGER,
+//   Marca VARCHAR(30)
+//   )";
+
+$query = "CREATE TABLE FORMA_PAGTO (
+  ID_Forma_Pagto INTEGER PRIMARY KEY,
+  Descricao VARCHAR(30)
+  );
+  CREATE TABLE COMPRA_PRODUTO (
+  ID_Compra_Produto INTEGER PRIMARY KEY,
+  QTD_Comprada INTEGER,
+  VL_Total_Item NUMERIC(7,2),
+  ID_Produto INTEGER,
+  FOREIGN KEY(ID_Produto) REFERENCES PRODUTO (ID_Produto)
+  );
+  CREATE TABLE COMPRA (
+  ID_Compra INTEGER PRIMARY KEY,
+  DT_Compra DATE,
+  VL_Total_Compra NUMERIC(7,2),
+  Atendente VARCHAR(50),
+  ID_Forma_Pagto INTEGER,
+  ID_Cliente INTEGER,
+  ID_Compra_Produto INTEGER,
+  FOREIGN KEY(ID_Cliente) REFERENCES CLIENTE (ID_Cliente),
+  FOREIGN KEY(ID_Forma_Pagto) REFERENCES FORMA_PAGTO (ID_Forma_Pagto),
+  FOREIGN KEY(ID_Compra_Produto) REFERENCES COMPRA_PRODUTO (ID_Compra_Produto)
+  )";
+
+// $query = "EXPLAIN cliente";
 
 if (mysqli_query($bdConection, $query)) {
   echo "Query realizada com sucesso";
