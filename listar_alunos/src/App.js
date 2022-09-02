8906e798b22ac157214d5d65f7e9e4c3bd741cd8
Elizeu <el_vailant@hotmail.com>
2022-09-02 16:41:37 -0400

    começo listas alunos

--- listar_alunos/src/App.js
+++ listar_alunos/src/App.js
@@ -2,9 +2,34 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Listar de alunos.</h1>
-
+    <div>
+      <h1>Listar alunos.</h1>
+      <form>
+        <label htmlFor="nome">Nome do aluno:</label>
+          <input 
+            type="text" 
+            id="nome"
+            name="nome" 
+          />
+        <label htmlFor="curso">Nome do curso:</label>
+          <input 
+            type="text" 
+            id="curso"
+            name="curso" 
+          />
+        <label htmlFor="periodo">Período:</label>
+          <input 
+            type="text" 
+            id="periodo"
+            name="periodo" 
+          />
+        <label htmlFor="disciplina">Disciplina:</label>
+          <input 
+            type="text" 
+            id="disciplina"
+            name="disciplina" 
+          />
+      </form>
     </div>
   );
 }
