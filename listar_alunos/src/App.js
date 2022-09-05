e7ff85f067c460f2b480f9baa38c2248bb4111d0
Elizeu <el_vailant@hotmail.com>
2022-09-04 21:57:07 -0400

    arrumando erros

--- listar_alunos/src/App.js
+++ listar_alunos/src/App.js
@@ -5,11 +5,44 @@ export default function App() {
     <div>
       <h1>Salvar alunos</h1>
       <form>
+        <label>
+          Nome
+          <input
+            type="text"
+            name="nome"
+          />
+        </label>
+        <br />
+        <label>
+          Curso
+          <input
+            type="text"
+            name="curso"
+          />
+        </label>
+        <br />
+        <label>
+          Semestre
+          <input
+            type="number"
+            name="semestre"
+          />
+        </label>
+        <br />
+        <label>
+          Disciplina
+          <input
+            type="text"
+            name="disciplina"
+          />
+        </label>
         <input
-          type="text"
-          name="nome"
+          type="submit"
+          value="Salvar"
         />
       </form>
+      <hr />
+      <ul></ul>
     </div>
   );
 }
