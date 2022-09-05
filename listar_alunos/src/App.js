17368b45874d04cab1140f464c738bf7e20671bc
Elizeu <el_vailant@hotmail.com>
2022-09-04 20:15:34 -0400

    erro arrumado

--- listar_alunos/src/App.js
+++ listar_alunos/src/App.js
@@ -1,68 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function App() {
-  const [Alunos, setAlunos] = useState([]);
-
-  const salvaAluno = () => {
-    setAlunos(
-      stateAnterior => {
-        return {}
-      }
-    );
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("Você salvou um aluno!");
-  }
-
   return (
     <div>
       <h1>Salvar alunos</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Nome do aluno:
-          <input
-            type="text"
-            name="nome" 
-          />
-        </label>
-        <br/>
-        <br/>
-        <label>Nome do curso:
-          <input 
-            type="text"
-            name="curso" 
-          />
-        </label>
-        <br/>
-        <br/>
-        <label>Período:
-          <input 
-            type="text"
-            name="periodo" 
-          />
-        </label>
-        <br/>
-        <br/>
-        <label>Disciplina:
-          <input 
-            type="text"
-            name="disciplina" 
-          />
-        </label>
-        <br/>
-        <br/>
-        <input type="submit">
-          Salvar
-        </input>
+      <form>
+        <input
+          type="text"
+          name="nome"
+        />
       </form>
-      <br/>
-      <hr/>
-      <h2>Lista de alunos</h2>
-      <br/>
-      <ul>
-
-      </ul>
     </div>
   );
 }
