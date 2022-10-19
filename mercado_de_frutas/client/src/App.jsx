3407326fee42bdb8de14b02135b8860de6592acb
Elizeu <el_vailant@hotmail.com>
2022-10-18 22:50:00 -0400

    separando componente navegador

--- mercado_de_frutas/client/src/App.jsx
+++ mercado_de_frutas/client/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, Route } from "react-router-dom";
 import Carrinho from "./paginas/Carrinho";
 import InformacaoFruta from "./paginas/InformacaoFruta";
 import MercadoDeFrutas from "./paginas/MercadoDeFrutas";
-import Navegador from "./paginas/Navegador";
+import Navegador from "./Componentes/Navegador";
 import PaginaErro from "./paginas/PaginaErro";
 
 const Roteador = createBrowserRouter([
--- mercado_de_frutas/client/src/Componentes/Navegador.jsx
+++ mercado_de_frutas/client/src/Componentes/Navegador.jsx
@@ -9,9 +9,6 @@ const Navegador = () => {
             <Link to="/">Mercado de Frutas</Link>
           </li>
           <li>
-            <Link to="/Informacoes">Informações da Fruta</Link>
-          </li>
-          <li>
             <Link to="/Carrinho">Carrinho</Link>
           </li>
         </ul>
